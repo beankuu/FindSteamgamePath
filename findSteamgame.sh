fd92038d96856155ff05d1e3e580dbe22e753e8d
beankuu <beankuu@gmail.com>
2021-01-12 17:29:17 -0700

    modified variable names

--- findSteamgame.sh
+++ findSteamgame.sh
@@ -1,30 +1,31 @@
+#!/usr/bin/env bash
 #Get INPUT
-MANIFESTNO=$1;
+manifest_no=$1;
 #Check INPUT is number
 #https://stackoverflow.com/questions/806906/how-do-i-test-if-a-variable-is-a-number-in-bash
 re='^[0-9]+$'
-if ! [[ $MANIFESTNO =~ $re ]] ; then
+if ! [[ $manifest_no =~ $re ]] ; then
    echo "Error: Invalid Input (not a number)" >&2; exit 1
 fi
 
 #find main Steam dir by using Reg Query(Windows)
-STEAMDIR=$(Reg Query 'HKCU\SOFTWARE\Valve\Steam' | grep SteamPath | xargs | cut -d " " -f3)/steamapps;
+steamdir=$(Reg Query 'HKCU\SOFTWARE\Valve\Steam' | grep SteamPath | xargs | cut -d " " -f3)/steamapps;
 #if game is installed in main Steam dir
-if [ -f "$STEAMDIR/appmanifest_$MANIFESTNO.acf" ];then 
-    WTDIR=$STEAMDIR;
+if [ -f "$steamdir/appmanifest_$manifest_no.acf" ];then 
+    wtdir=$steamdir;
 #if game is NOT installed in main Steam dir
 else
     #fetch list of library folder files located in main Steam dir
-    steamDirLists=$(cat $STEAMDIR/libraryfolders.vdf | sed -e '/^\"/d' -e '/^{/d' -e '/^}/d' | tail -n +3 | xargs | awk '{ for (i=1;i<=NF;i+=2) $i="" } 1' | xargs)
+    steamdir_list=$(cat $steamdir/libraryfolders.vdf | sed -e '/^\"/d' -e '/^{/d' -e '/^}/d' | tail -n +3 | xargs | awk '{ for (i=1;i<=NF;i+=2) $i="" } 1' | xargs)
     #loop through list and find the correct folder
-    for N in $steamDirLists; do
-        if [ -f "$N/steamapps/appmanifest_$MANIFESTNO.acf" ];then
-            WTDIR=$N/steamapps;
+    for N in $steamdir_list; do
+        if [ -f "$N/steamapps/appmanifest_$manifest_no.acf" ];then
+            wtdir=$N/steamapps;
             break;
         fi
     done
-    if [ -z "$WTDIR" ];then
+    if [ -z "$wtdir" ];then
         echo "Error: game not found"; >&2; exit 1
     fi
 fi;
-echo $WTDIR
+echo $wtdir
